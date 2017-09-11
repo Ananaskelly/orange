db52aacd5769977c85e40d577a7580abacd91404
janchk <janchk@gmail.com>
2017-09-11 21:57:11 +0300

    many filters

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 import time
 import os, re
+import matplotlib as plt
 
 start_time = time.time()
 directory = 'C:/Users/janch/Desktop/trainset/'
@@ -9,8 +10,53 @@ r = re.compile(".*avi")
 files = filter(r.match, [f for f in os.listdir(directory)])
 
 
-def rs(img):
+def rs1(img):
     return cv2.resize(img, (800, 600))
+
+def rs(img):
+    return cv2.resize(img, (320, 240))
+
+
+def zebra(img):
+    kernel1 = np.ones((5, 5), np.uint8)
+    lower_w = (150)
+    upper_w = (240)
+    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
+    kernel = (5, 5)
+    # img = cv2.GaussianBlur(img, kernel, 0)
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # clahed = gray_img
+    clahed = clahe.apply(gray_img)
+    clahed = cv2.GaussianBlur(clahed, kernel, 0)
+    cv2.imshow('gray', rs(gray_img))
+    gray_img = persp_transformation(clahed)[0]  # gray_img
+    cv2.imshow('clahed_transform', rs(gray_img))
+
+    # base thresh
+    th = cv2.inRange(gray_img, lower_w, upper_w)
+    # reth, th1 = cv2.threshold(gray_img, lower_w, upper_w, cv2.THRESH_OTSU)
+
+    # erosed = cv2.erode(th1, kernel, iterations=10)
+    # cv2.imshow('OTSU erode', rs(erosed))
+    # cv2.imshow('OTSU opening', rs(closing))
+
+    th2 = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    closing = cv2.morphologyEx(th2, cv2.MORPH_CLOSE, kernel1)
+
+    # hls_img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    # l = hls_img[:, :, 1]
+    # s = hls_img[:, :, 2]
+    # binary_output = np.zeros_like(s)
+    # binary_output[(s >= sthresh[0]) & (s <= sthresh[1])
+    #               & (l >= lthresh[0]) & (l <= lthresh[1])] = 255
+    cv2.imshow('mean_thrsh', rs(th2))
+    cv2.imshow('base thrashold', rs(th))
+    cv2.imshow('adapt open', rs1(th2))
+    # cv2.imshow('clahed', rs(clahed))
+    # cv2.imshow('thrashed2', rs(th2))
+    # cv2.imshow('bin', rs(binary_output))
+    # cv2.imshow('frame', rs(img))
+    # return binary_output
 
 
 def persp_transformation(img):
@@ -18,11 +64,16 @@ def persp_transformation(img):
     y = img.shape[0]
     img_size = (x, y)
     source_points = np.float32([
-    [0.117 * x, y],
-    [(0.5 * x) - (x*0.078), (2/3)*y],
-    [(0.5 * x) + (x*0.078), (2/3)*y],
-    [x - (0.117 * x), y]
+        [0.217 * x, y],
+        [(0.5 * x) - (x * 0.068), (3 / 4) * y],
+        [(0.5 * x) + (x * 0.068), (3 / 4) * y],
+        [x - (0.217 * x), y]
     ])
+    # [0.117 * x, y],
+    # [(0.5 * x) - (x*0.078), (2/3)*y],
+    # [(0.5 * x) + (x*0.078), (2/3)*y],
+    # [x - (0.117 * x), y]
+    # ])
 
     destination_points = np.float32([
         [0.25 * x, y],
@@ -45,11 +96,13 @@ def processing(capture):
     while True:
         try:
             if frame_count % frame_skip == 0:
-                    ret, frame = cap.read()
-            cv2.imshow('transformed', rs(persp_transformation(frame)[0]))
-            cv2.imshow('frame', rs(frame))
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                print(-1)
+                ret, frame = cap.read()
+                zebra(frame)
+                cv2.imshow('orig', rs(frame))
+            # cv2.imshow('transformed', rs(persp_transformation(frame)[0]))
+            # cv2.imshow('frame', rs(frame))
+            if cv2.waitKey(30) & 0xFF == ord('q'):
+                # print(-1)
                 break
         except:
             return 0
@@ -60,6 +113,7 @@ for file in files:
     filepath = (directory + file)
     cap = cv2.VideoCapture(filepath)
     start_ret, start_frame = cap.read()
+
 
     processing(cap)
 
